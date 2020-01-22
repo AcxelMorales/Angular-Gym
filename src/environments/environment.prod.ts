4eb93ee2bfb8999574aee4881c7798e5419ef7d6
Acxel Morales <39923586+AcxelMorales@users.noreply.github.com>
2020-01-21 21:23:52 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyAma3HOdbaFJ1YT9WtkPXbKRSIWbQDt9p8",
-    authDomain: "angular-gym.firebaseapp.com",
-    databaseURL: "https://angular-gym.firebaseio.com",
-    projectId: "angular-gym",
-    storageBucket: "angular-gym.appspot.com",
-    messagingSenderId: "758490992793",
-    appId: "1:758490992793:web:0e9edd3203df4718c6e808"
-  }
+  production: true
 };
