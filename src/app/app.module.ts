c7f6167489b1fb1f5a1a4641d5e53dd13e068f9d
Acxel Morales <acxelmorales97@gmail.com>
2020-01-21 21:07:22 -0600

    Configuracion de Firebase, Bootstrap y Ngx-Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment.prod';
+
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
